7052f612226cfeca6a9ceea9147a42a96a0fffc4
Soham Jadhav <soham.ai.engineer@gmail.com>
2024-07-24 12:46:50 +0530

    Exception/Error Handling Feature Added

--- Project.py
+++ Project.py
@@ -1,31 +1,66 @@
 import os
 
-def Create_File():
-    filename = str(input("Enter the filename to create: "))
-    file = open(filename, 'x')
-    
-def Read_File():
-    filename = str(input("Enter the filename to read: "))
-    file = open(filename, 'r')
-    print(f"Content in file: {file.read()}")
-    file.close()
-    
-def Edit_File():
-    filename = str(input("Enter the filename to edit: "))
-    file = open(filename, 'w')
-    cont =  str(input("Enter the Content: "))
-    write = file.write(cont)
-    print(f"Content written: {write}")
-    
-def Delete_File():
-    filename = str(input("Enter the filename to delete: "))
-    file = os.remove(filename)
-    print(f"deleted: {filename}")
-    
-def view_file():
-    files = os.listdir()
-    for f in files:
-        print(f"Files in Directory: {f}")
+while True:
+    def Create_File():
+        try:
+            filename = str(input("Enter the filename to create: "))
+            file = open(filename, 'x')
+            
+        except FileExistsError:
+            print(f"{filename} file alredy Exist")
+            
+        except Exception as e:
+            print("An unknown error occurred, please try again")
+        
+    def Read_File():
+            
+        try:
+            filename = str(input("Enter the filename to read: "))
+            file = open(filename, 'r')
+            print(f"Content in file: \n {file.read()}")
+            file.close()
+        except FileNotFoundError:
+            print("File Not Found, please try again")
+            
+        except Exception as e:
+            print("An unknown error occurred, please try again")
+            
+    def Edit_File():
+        
+        try:
+            filename = str(input("Enter the filename to edit: "))
+            file = open(filename, 'w')
+            cont =  str(input("Enter the Content: "))
+            write = file.write(cont)
+            print(f"Content written: {write}")
+        except FileNotFoundError:
+            print("File Not Found, please try again")
+        except Exception as e:
+            print("An unknown error occurred, please try again")
+        
+    def Delete_File():
+            
+        try:
+            filename = str(input("Enter the filename to delete: "))
+            file = os.remove(filename)
+            print(f"deleted: {filename}")
+        except FileNotFoundError:
+            print("File Not Found, please try again")
+        except Exception as e:
+            print("An unknown error occurred, please try again")
+            
+    def view_file():
+        
+        if not files:
+            print("No Files Found")
+            
+        else:
+            files = os.listdir()
+            for f in files:
+                print(f"Files in Directory: {f}")
+                
+    break
+        
         
 
         
